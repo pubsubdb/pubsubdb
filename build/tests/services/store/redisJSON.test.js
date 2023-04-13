77c51e0145ffda52803b38dc8e86bdd679b76232
Luke Birdeau <luke.birdeau@gmail.com>
2023-04-13 15:21:52 -0700

    removes build; bumps version to 003

--- build/tests/services/store/redisJSON.test.js
+++
@@ -1,7 +0,0 @@
-describe('RedisJSONStoreService', () => {
-    describe('getKey', () => {
-        it('should return the key for the given namespace and key for the store', () => {
-            expect(1).toEqual(1);
-        });
-    });
-});
