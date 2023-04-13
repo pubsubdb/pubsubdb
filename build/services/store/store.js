0ad3451bad781ffcf89dbeb8a91d94f1c35957cd
Luke <luke.birdeau@gmail.com>
2023-04-13 15:28:12 -0700

    removes build; bumps version to 003 (#19)

--- build/services/store/store.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.StoreService = void 0;
-class StoreService {
-}
-exports.StoreService = StoreService;
--- build/typedefs/pubsubdb.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RedisStoreService = exports.StoreService = void 0;
-const redis_1 = require("../services/store/redis");
-Object.defineProperty(exports, "RedisStoreService", { enumerable: true, get: function () { return redis_1.RedisStoreService; } });
-const store_1 = require("../services/store/store");
-Object.defineProperty(exports, "StoreService", { enumerable: true, get: function () { return store_1.StoreService; } });
